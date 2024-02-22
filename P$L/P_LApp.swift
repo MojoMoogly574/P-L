720147c28219692c65b80d4b8381f19e5eeb0556
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-02-22 11:47:29 -0600

    Initial Commit

---
+++ P$L/P_LApp.swift
@@ -0,0 +1,17 @@
+//
+//  P_LApp.swift
+//  P$L
+//
+//  Created by J. DeWeese on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct P_LApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
